23ca7eaf6a8c27320fa455b9175a0def5283d432
cyberbeast <sandeshgade@gmail.com>
2016-10-29 18:01:20 -0400

    UI Scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tool } from './tool';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PhantomOblivion';
+  title_one = 'Phantom';
+  title_two = 'Oblivion';
+
+  home_dashboard_tools = [
+  	new Tool(1, 'VM Manager', 'Tab for VM Manager'),
+  	new Tool(2, 'TOR Manager', 'Tab for TOR Manager'),
+  	new Tool(3, 'WebApp Manager', 'Tab for WebApp Manager')
+  ];
 }
+
---
+++ src/app/tool.ts
@@ -0,0 +1,7 @@
+export class Tool {
+	constructor(
+		public id: number,
+		public name: string,
+		public description: string
+		){}
+}
