acf69c6becfce8f6ce747db27498ced7b5a8baac
cyberbeast <sandeshgade@gmail.com>
2016-11-07 20:41:08 -0500

    dash component service with observables

--- server/hug_app/config_api.py
+++ server/hug_app/config_api.py
@@ -1,5 +1,11 @@
 import hug
+def cors_support(response, *args, **kwargs):
+    response.set_header('Access-Control-Allow-Origin', '*')
 
 @hug.get('/')
 def trial():
 	return 'Hello World! from config_api'
+
+@hug.get('/get_tabs', requires=cors_support)
+def get_tabs():
+	return [{'id': 1, 'name': 'VM Manager', 'description': 'desc for vm manager tab'}, {'id': 2, 'name': 'TOR Manager', 'description': 'desc for TOR manager tab'}, {'id': 3, 'name': 'Web Services Manager', 'description': 'desc for Web Services manager tab'}]
--- server/run.py
+++ server/run.py
@@ -2,10 +2,16 @@
 
 import hug
 from hug_app import config_api, vm_management_api
+import json
+
+@hug.not_found()
+def not_found_handler():
+    return "Thank you for your interest in our backend processes. But the resource you are looking/snooping for is not available here!"
 
 @hug.get('/')
 def say_hi():
-    return "Server says HI! :P"
+	response = {"Hello World from server"}
+	return response
 
 @hug.extend_api('/config')
 def config_api():
